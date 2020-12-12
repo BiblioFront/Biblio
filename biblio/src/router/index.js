2185c1d2496733634080b41d94e66aaac67a30c4
unknown <568385476@qq.com>
2020-12-12 10:08:43 +0800

    文献打开页面

--- biblio/src/router/index.js
+++ biblio/src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Gate from "../views/Gate.vue";
 import Search from "../views/Search.vue";
+import Paper from "../views/paper.vue";
+import PaperDetail from "../views/paperdetail.vue";
 
 Vue.use(VueRouter);
 
@@ -22,6 +24,16 @@ const routes = [
     name: "Search",
     component: Search,
   },
+  {
+    path: "/paper",
+    name: "Paper",
+    component: Paper
+  },
+  {
+    path: "/paperdetail",
+    name: "PaperDetail",
+    component :PaperDetail
+  }
 ];
 
 const router = new VueRouter({
