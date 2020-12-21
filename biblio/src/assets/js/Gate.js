935e8469b28fb1e384a17bbdf6f3ff27c943031d
ka233-stack <1183493652@qq.com>
2020-12-21 14:41:54 +0800

    认证

--- biblio/src/assets/js/Gate.js
+++ biblio/src/assets/js/Gate.js
@@ -1,12 +1,12 @@
 /* Gate.js */
-import Nav from "@/components/Nav.vue";
-import WordsCloud from "@/components/WordsCloud.vue";
-import LineMap from "@/components/LineMap.vue";
-import Collaborator from "@/components/Collaborator.vue";
-import CircleProgressbar from "@/components/CircleProgressbar.vue";
+import Nav from '@/components/Nav.vue'
+import WordsCloud from '@/components/WordsCloud.vue'
+import LineMap from '@/components/LineMap.vue'
+import Collaborator from '@/components/Collaborator.vue'
+import CircleProgressbar from '@/components/CircleProgressbar.vue'
 
 export default {
-  name: "Gate",
+  name: 'Gate',
   components: {
     Nav,
     WordsCloud,
@@ -16,53 +16,82 @@ export default {
   },
   data() {
     return {
+      hasSendEmail: false, // 判断是否已经发送过邮件
+      verificationBoxVisible: true, // 验证框可见性
+      verificationDialogVisible: false, // 验证弹窗可见性
+      verificationErrorVisible: false, // 验证错误信息可见性
+      verificationErrorInfo: '发生了一些错误...', // 验证错误信息
+      // 验证表单
+      verificationForm: {
+        verification_code: '',
+      },
+      uploadAchievementDialogVisible: false, // 上传学术成果弹窗可见性
+      uploadPaperForm: {},
+      uploadPatentForm: {},
+      uploadProjectForm: {},
+      achievementType: null,
+      options: [
+        {
+          value: '论文',
+          label: '论文',
+        },
+        {
+          value: '专利',
+          label: '专利',
+        },
+        {
+          value: '研究项目',
+          label: '研究项目',
+        },
+      ],
+
       scholarKeywords: [
         {
-          name: "十九大精神",
+          name: '十九大精神',
           value: 15000,
         },
         {
-          name: "两学一做",
+          name: '两学一做',
           value: 10081,
         },
         {
-          name: "中华民族",
+          name: '中华民族',
           value: 9386,
         },
         {
-          name: "马克思主义",
+          name: '马克思主义',
           value: 7500,
         },
         {
-          name: "民族复兴",
+          name: '民族复兴',
           value: 7500,
         },
         {
-          name: "社会主义制度",
+          name: '社会主义制度',
           value: 6500,
         },
         {
-          name: "国防白皮书",
+          name: '国防白皮书',
           value: 6500,
         },
         {
-          name: "创新",
+          name: '创新',
           value: 6000,
         },
         {
-          name: "民主革命",
+          name: '民主革命',
           value: 4500,
         },
         {
-          name: "文化强国",
+          name: '文化强国',
           value: 3800,
         },
         {
-          name: "国家主权",
+          name: '国家主权',
           value: 3000,
         },
         {
-          name: "武装颠覆",
+          name: '武装颠覆',
           value: 2500,
         },
       ],
@@ -83,12 +112,238 @@ export default {
       ],
       biblioIndex: [5, 1, 4, 3, 2, 20, 2, 3, 14, 11, 12, 10, 5],
       collaborators: [
-        { value: 235, name: "视频广告" },
-        { value: 274, name: "联盟广告" },
-        { value: 310, name: "邮件营销" },
-        { value: 335, name: "直接访问" },
-        { value: 400, name: "搜索引擎" },
+        { value: 235, name: '视频广告' },
+        { value: 274, name: '联盟广告' },
+        { value: 310, name: '邮件营销' },
+        { value: 335, name: '直接访问' },
+        { value: 400, name: '搜索引擎' },
       ],
-    };
+      all: {
+        isFollow: false, // 当前用户是否关注该学者 bool
+        isAuth: 0, // 当前门户的认证情况： 0表示未被认证，1表示已被认证但不是当前用户认证，2表示已被认证且被当前用户认证
+        publishYear: {
+          // 论文的出版年统计
+          '2011': 1,
+          '2010': 1,
+        },
+        paper: {
+          // 论文信息
+          msg: 'Search success',
+          response: 'success',
+          info: [
+            {
+              _id: '5fccd7df376ae34bbb980e61',
+              title: '面向分布式系统访问控制的信任度量化模型',
+              author: '郎波',
+              summary:
+                '通过引入社会学中信任的研究成果,定义了分布式系统访问控制中信任的具体语义、基本特性与上下文相关属性.针对信任的主观性、模糊性与不确定性,采用模糊数学与概率论相结合的方法,给出了信任的数学定义,建立了包括信任综合评价与信任计算的信任度量化模型.该模型综合了访问控制背景下信任相关的各种因素,符合推荐信任的衰减与增强规律.',
+              url: null,
+              keywords: '访问控制;信任量化表达;信任计算;分布式系统',
+              date: '2011-03-24',
+              journal: '通信学报',
+              organization:
+                '北京航空航天大学,软件开发环境国家重点实验室,北京,100083',
+              likes: 0,
+              read: 0,
+              change: false,
+              doi: '10.3969/j.issn.1000-436X.2010.12.006',
+            },
+            {
+              _id: '5fcee2e700f8a0090a698822',
+              title: '网格计算中一种基于属性的访问控制方法',
+              author: '倪文婷,郎波',
+              summary:
+                '为了迎合目前网格计算对动态、细粒度授权的需求,针对网格资源分层式的组织结构特点,在现有的基于属性的访问控制(ABAC)模型的基础上,提出了一种针对网格资源的ABAC模型Grid_ABAC,并设计了基于XACML的Grid_ABAC实现框架,应用在GT4平台上.最后对GT4中的应用作了测试,测试结果表明授权结果与预期结果相同,且时间开销随着规模的增长并没有增长很多,在可接受范围内.证明Grid_ABAC模型在网格平台上具有一定的实用性.',
+              url: null,
+              keywords:
+                '网格计算;Globus网格平台;基于属性的访问控制;可扩展访问控制标记语言',
+              date: '2010-04-06',
+              journal: '计算机应用研究',
+              organization:
+                '北京航空航天大学,计算机学院,软件开发环境国家重点实验室,北京,100086',
+              likes: 0,
+              read: 0,
+              change: false,
+              doi: '10.3969/j.issn.1001-3695.2010.02.082',
+            },
+          ],
+        },
+        patent: {
+          // 专利信息
+          msg: 'Search success',
+          response: 'success',
+          info: [],
+        },
+        project: {
+          // 科研项目信息
+          msg: 'Search success',
+          response: 'success',
+          info: [],
+        },
+        wordCloud: {
+          // 词云统计
+          信任量化表达: 1,
+          基于属性的访问控制: 1,
+          信任计算: 1,
+          网格计算: 1,
+          分布式系统: 1,
+          访问控制: 1,
+          可扩展访问控制标记语言: 1,
+          Globus网格平台: 1,
+        },
+        scholarInfo: {
+          // 当前门户的学者信息
+          scholarID: '5fccd7df376ae34bbb980e61',
+          name: '郎波',
+          field: null,
+          organization: '北京航空航天大学',
+          userID: null,
+          id: '5fdf795e35f0973eaf28b1e1',
+        },
+        relateScholar: {
+          // 合作学者统计
+          倪文婷: 1,
+        },
+      },
+    }
   },
-};
+  methods: {
+    // 发送验证邮件
+    verification() {
+      this.verificationDialogVisible = true
+      this.verificationBoxVisible = false
+      if (!this.hasSendEmail) {
+        var scholarID = this.all.scholarInfo.scholarID
+        console.log(scholarID)
+        var token = window.localStorage.getItem('token')
+        console.log(token)
+
+        this.$axios
+          .post(
+            '/user/verification?researcherID=' + scholarID,
+            { verification_code: this.verificationForm.verification_code },
+            { headers: { token: token } }
+          )
+          .then((response) => {
+            var msg = response.data.msg
+            console.log(msg)
+            if (msg === 'Send verification code successfully') {
+              this.hasSendEmail = true
+              this.verificationBoxVisible = true
+            } else if (msg === 'Email format error') {
+              this.verificationErrorVisible = true
+              this.verificationErrorInfo = '邮箱格式错误'
+              this.$message.error('邮箱格式错误')
+            } else if (msg === 'Unsupported mailbox') {
+              this.verificationErrorVisible = true
+              this.verificationErrorInfo = '不支持您的邮箱类型'
+              this.$message.error('不支持您的邮箱类型')
+            } else if (msg === 'please login first') {
+              this.verificationErrorVisible = true
+              this.verificationErrorInfo = '请先登录'
+              this.$message.error('请先登录')
+            }
+          })
+          .catch((error) => {
+            console.log(error)
+            console.log('请求异常')
+          })
+      } else {
+        this.verificationBoxVisible = true
+      }
+    },
+    // 发送验证码
+    claim() {
+      var scholarID = this.all.scholarInfo.scholarID
+      console.log(scholarID)
+      var token = window.localStorage.getItem('token')
+      console.log(token)
+
+      this.$axios
+        .post(
+          '/user/claim?researcherID=' + scholarID,
+          { verification_code: this.verificationForm.verification_code },
+          { headers: { token: token } }
+        )
+        .then((response) => {
+          var msg = response.data.msg
+          console.log(msg)
+          if (msg === 'claim successfully') {
+            this.$message.success('认证成功')
+            this.verificationDialogClosed()
+          } else if (msg === 'Wrong verification code') {
+            this.$message.error('验证码错误')
+          } else if (msg === 'please login first') {
+            this.verificationErrorVisible = true
+            this.verificationErrorInfo = '请先登录'
+            this.$message.error('请先登录')
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          console.log('请求异常')
+        })
+    },
+    // 关闭验证弹窗
+    verificationDialogClosed() {
+      this.verificationDialogVisible = false
+      this.verificationErrorVisible = false
+      // this.$refs.verificationFormRef.resetFields()
+      this.verificationForm.verification_code = ''
+    },
+    // 上传学术成果
+    uploadAchievement() {
+      console.log(this.achievementType)
+      if (this.achievementType === '论文') {
+        let uploadPaperForm = {
+          title: this.uploadPaperForm.title,
+          summary: this.uploadPaperForm.summary,
+          url: this.uploadPaperForm.url,
+          keywords: this.uploadPaperForm.keywords,
+          journal: this.uploadPaperForm.journal,
+          organization: this.uploadPaperForm.organization,
+        }
+        console.log(uploadPaperForm)
+      } else if (this.achievementType === '专利') {
+        let uploadPatentForm = {
+          title: this.uploadPatentForm.title,
+          summary: this.uploadPatentForm.summary,
+          type: this.uploadPatentForm.type,
+          patentID: this.uploadPatentForm.patentID,
+          applyDate: this.uploadPatentForm.applyDate,
+          publishID: this.uploadPatentForm.publishID,
+          publicDate: this.uploadPatentForm.publicDate,
+          mainTypeNumber: this.uploadPatentForm.mainTypeNumber,
+          typeNumber: this.uploadPatentForm.typeNumber,
+          owner: this.uploadPatentForm.owner,
+          designer: this.uploadPatentForm.designer,
+          address: this.uploadPatentForm.address,
+          agency: this.uploadPatentForm.agency,
+          agent: this.uploadPatentForm.agent,
+          code: this.uploadPatentForm.code,
+          description: this.uploadPatentForm.description,
+          status: this.uploadPatentForm.status,
+        }
+
+        console.log(uploadPatentForm)
+      } else if (this.achievementType === '研究项目') {
+        let uploadProjectForm = {
+          author: this.uploadProjectForm.author,
+          title: this.uploadProjectForm.title,
+          summary: this.uploadProjectForm.summary,
+          company: this.uploadProjectForm.company,
+          keywords: this.uploadProjectForm.keywords,
+          year: this.uploadProjectForm.year,
+          scope: this.uploadProjectForm.scope,
+          plan: this.uploadProjectForm.plan,
+        }
+        console.log(uploadProjectForm)
+      }
+    },
+    uploadAchievementDialogOpened() {
+      this.achievementType = null
+      this.uploadPaperForm = {}
+      this.uploadAchievementDialogVisible = true
+    },
+  },
+}
--- biblio/src/views/Gate.vue
+++ biblio/src/views/Gate.vue
@@ -19,8 +19,11 @@
         </div>
 
         <div class="scholar_btns_area">
-          <button>我要认证</button>
+          <button @click="verification">我要认证</button>
           <button>关注</button>
+          <button @click="uploadAchievementDialogOpened">
+            上传成果
+          </button>
         </div>
       </div>
 
@@ -46,6 +49,193 @@
         <Collaborator :data="collaborators" />
       </div>
     </div>
+
+    <el-dialog
+      title="认证"
+      :visible.sync="verificationDialogVisible"
+      @close="verificationDialogClosed"
+    >
+      <div
+        v-loading="!verificationBoxVisible"
+        element-loading-text="正在向您的邮箱发送验证邮件"
+        v-show="!verificationErrorVisible"
+      >
+        <transition name="el-fade-in-linear">
+          <div v-show="verificationBoxVisible" class="verification-box">
+            <div>
+              <span>
+                我们已向您的注册邮箱发送了验证码，请注意查收
+              </span>
+            </div>
+            <el-input
+              v-model="verificationForm.verification_code"
+              placeholder="验证码"
+            ></el-input>
+          </div>
+        </transition>
+      </div>
+      <div v-show="verificationErrorVisible">
+        <img src="../assets/img/error.png" width="100" alt="" />
+        <span>{{ verificationErrorInfo }}</span>
+      </div>
+
+      <div slot="footer" class="dialog-footer" v-show="verificationBoxVisible">
+        <el-button @click="claim">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog
+      title="上传学术成果"
+      :visible.sync="uploadAchievementDialogVisible"
+      @close="verificationDialogClosed"
+    >
+      <div>
+        <el-select v-model="achievementType" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </div>
+      <div v-show="achievementType == '论文'">
+        <span>论文题目</span>
+        <el-input v-model="uploadPaperForm.title" placeholder="题目">
+        </el-input>
+        <span>论文摘要</span>
+        <el-input
+          type="textarea"
+          autosize
+          v-model="uploadPaperForm.summary"
+          placeholder="摘要"
+        >
+        </el-input>
+        <span>论文链接</span>
+        <el-input v-model="uploadPaperForm.url" placeholder="链接"> </el-input>
+        <span>关键词</span>
+        <el-input v-model="uploadPaperForm.keywords" placeholder="关键词">
+        </el-input>
+        <span>期刊</span>
+        <el-input v-model="uploadPaperForm.journal" placeholder="期刊">
+        </el-input>
+        <span>机构</span>
+        <el-input v-model="uploadPaperForm.organization" placeholder="机构">
+        </el-input>
+      </div>
+      <div v-show="achievementType == '专利'">
+        <span>专利名</span>
+        <el-input v-model="uploadPatentForm.title" placeholder="专利名">
+        </el-input>
+        <span>专利类型</span>
+        <el-input v-model="uploadPatentForm.patentID" placeholder="类型">
+        </el-input>
+        <span>申请日期</span>
+        <el-date-picker
+          v-model="uploadPatentForm.applyDate"
+          type="date"
+          placeholder="选择日期"
+          value-format="yyyy-MM-dd"
+        >
+        </el-date-picker>
+        <span>公开号</span>
+        <el-input v-model="uploadPatentForm.publishID" placeholder="专利公开号">
+        </el-input>
+        <span>公开日期</span>
+        <el-date-picker
+          v-model="uploadPatentForm.publicDate"
+          type="date"
+          placeholder="选择日期"
+          value-format="yyyy-MM-dd"
+        >
+        </el-date-picker>
+        <span>主分类号</span>
+        <el-input
+          v-model="uploadPatentForm.mainTypeNumber"
+          placeholder="主分类号"
+        >
+        </el-input>
+        <span>分类号</span>
+        <el-input v-model="uploadPatentForm.typeNumber" placeholder="分类号">
+        </el-input>
+        <span>专利权人</span>
+        <el-input v-model="uploadPatentForm.owner" placeholder="专利权人">
+        </el-input>
+        <span>专利发明人</span>
+        <el-input v-model="uploadPatentForm.designer" placeholder="专利发明人">
+        </el-input>
+        <span>主申请人地址</span>
+        <el-input v-model="uploadPatentForm.address" placeholder="地址">
+        </el-input>
+        <span>主申请人地址</span>
+        <el-input v-model="uploadPatentForm.address" placeholder="地址">
+        </el-input>
+        <span>代理机构</span>
+        <el-input v-model="uploadPatentForm.agency" placeholder="代理机构">
+        </el-input>
+        <span>代理人</span>
+        <el-input v-model="uploadPatentForm.agent" placeholder="代理人">
+        </el-input>
+        <span>国别、省市代码</span>
+        <el-input v-model="uploadPatentForm.code" placeholder="国别;省市代码">
+        </el-input>
+        <span>主权项</span>
+        <el-input
+          type="textarea"
+          autosize
+          v-model="uploadPatentForm.description"
+          placeholder="描述"
+        >
+        </el-input>
+        <span>法律状态</span>
+        <div>
+          <el-radio v-model="uploadPatentForm.status" label="有权">
+            有权
+          </el-radio>
+          <el-radio v-model="uploadPatentForm.status" label="无权">
+            无权
+          </el-radio>
+        </div>
+      </div>
+      <div v-show="achievementType == '研究项目'">
+        <span>参与者</span>
+        <el-input
+          v-model="uploadProjectForm.author"
+          placeholder="参与者，多个作者之间用‘，’隔开"
+        >
+        </el-input>
+        <span>标题</span>
+        <el-input v-model="uploadProjectForm.title" placeholder="标题">
+        </el-input>
+        <span>摘要</span>
+        <el-input
+          type="textarea"
+          autosize
+          v-model="uploadProjectForm.summary"
+          placeholder="摘要"
+        >
+        </el-input>
+        <span>工作单位</span>
+        <el-input
+          v-model="uploadProjectForm.company"
+          placeholder="多个单位之间用‘，’隔开，且与参与者一一对相应"
+        >
+        </el-input>
+        <span>关键词</span>
+        <el-input v-model="uploadProjectForm.keywords" placeholder="关键词">
+        </el-input>
+        <span>立项年份</span>
+        <el-input v-model.number="uploadProjectForm.year" placeholder="立项年份">
+        </el-input>
+        <span>公开范围</span>
+        <el-input v-model="uploadProjectForm.scope" placeholder="公开范围">
+        </el-input>
+      </div>
+      <div slot="footer" class="dialog-footer" v-show="verificationBoxVisible">
+        <el-button @click="uploadAchievement">确 定</el-button>
+      </div>
+    </el-dialog>
 
     <div class="achievements_box">
       <div id="filter_bar">
@@ -152,7 +342,7 @@
 <script src="../assets/js/Gate.js"></script>
 
 <style scoped>
-@import "../assets/css/global.css";
-@import "../assets/css/gate_main.css";
-@import "../assets/css/achievements.css";
+@import '../assets/css/global.css';
+@import '../assets/css/gate_main.css';
+@import '../assets/css/achievements.css';
 </style>
